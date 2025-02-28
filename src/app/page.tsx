69b785aeee63371098d96fee40b7882103c28b60
Bjoern <bjoern.wagner@gmail.com>
2025-02-28 21:36:17 +0100

    blink scaffold for solana

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,78 @@
-import Image from "next/image";
+"use client";
+
+import { Blink, useAction } from "@dialectlabs/blinks";
+import { useActionSolanaWalletAdapter } from "@dialectlabs/blinks/hooks/solana";
+import "@dialectlabs/blinks/index.css";
+
+import { StepCard } from "./components/step-card";
+
+// Text for the steps on the left side of the page for the user to follow
+const steps = [
+  {
+    icon: "icon-cog",
+    chip: {
+      text: "Backend",
+      icon: "icon-cog",
+    },
+    headline: "Blink API",
+    text: "Blinks are headless APIs that return transactions, as well as educational metadata that can be used to render blink UIs. \n\nGet started by editing `/src/app/api/actions/donate-sol/route.ts`",
+  },
+  {
+    icon: "icon-code",
+    chip: {
+      text: "Frontend",
+      icon: "icon-code",
+    },
+    headline: "Blink UI",
+    text: "Dialect's blinks UI components libraries can be used to render the blink data returned from the blink API backend. \n\nGet started by editing `src/app/page.tsx`",
+  },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // ConnectKit modal
+  const actionApiUrl = "http://localhost:3000/api/actions/donate-sol";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  // Adapter, used to connect to the wallet
+  const { adapter } = useActionSolanaWalletAdapter(
+    "https://api.devnet.solana.com"
+  );
+
+  // Action we want to execute in the Blink
+  const { action, isLoading } = useAction({ url: actionApiUrl });
+
+  return (
+    <main className="grid grid-cols-[2fr_3fr] h-[calc(100vh-64px)]">
+      <div className="col-span-1 p-8 pr-16 overflow-y-auto">
+        <h1 className="text-[40px] mb-3 font-bold leading-[1]">
+          Solana Blinks Starter Template
+        </h1>
+        <h2 className="text-[18px] mb-2">
+          Use this template project to get started developing your blink.
+        </h2>
+        {steps.map((step, i) => (
+          <StepCard
+            key={i}
+            chip={step.chip}
+            headline={step.headline}
+            text={step.text}
+          />
+        ))}
+      </div>
+
+      <div className=" flex items-center justify-center border border-gray-600 rounded-[10px] m-4">
+        {isLoading || !action ? (
+          <span>Loading</span>
+        ) : (
+          <div className="w-full max-w-lg">
+            <Blink
+              action={action}
+              adapter={adapter}
+              securityLevel="all"
+              stylePreset="x-dark"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
