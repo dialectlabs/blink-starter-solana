8646ac7f4e1d6968510ddde461173e1c1077553b
bjoern.sol <bjoern.wagner@gmail.com>
2025-04-30 14:21:53 +0200

    feat: landing optimized for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,12 +40,12 @@ export default function Home() {
   const { blink, isLoading } = useBlink({ url: blinkApiUrl });
 
   return (
-    <main className="grid grid-cols-[2fr_3fr] h-[calc(100vh-64px)]">
-      <div className="col-span-1 p-8 pr-16 overflow-y-auto">
-        <h1 className="text-[40px] mb-3 font-bold leading-[1]">
+    <main className="grid grid-cols-1 lg:grid-cols-[2fr_3fr] min-h-[calc(100vh-64px)]">
+      <div className="col-span-1 p-4 lg:p-8 lg:pr-16 lg:overflow-y-auto">
+        <h1 className="text-[32px] lg:text-[40px] mb-3 font-bold leading-[1]">
           Solana Blinks Starter Template
         </h1>
-        <h2 className="text-[18px] mb-2">
+        <h2 className="text-[16px] lg:text-[18px] mb-2">
           Use this template project to get started developing your blink.
         </h2>
         {steps.map((step, i) => (
@@ -58,7 +58,7 @@ export default function Home() {
         ))}
       </div>
 
-      <div className=" flex items-center justify-center border border-gray-600 rounded-[10px] m-4">
+      <div className="flex items-center justify-center lg:border lg:border-gray-600 rounded-[10px] m-4">
         {isLoading || !blink ? (
           <span>Loading</span>
         ) : (
